f9598050e7c2bc8401946890f828a45571a12d2c
Liangyizhou <461650578@qq.com>
2020-03-13 14:17:09 +0800

    task3-1

---
+++ task3-1/main.cpp
@@ -0,0 +1,50 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	VideoCapture cap(0);
+
+	double scale = 0.5;
+
+	//0-180
+	//·ôÉ«
+	double i_minH = 0;
+	double i_maxH = 20;
+	//0-255
+	double i_minS = 43;
+	double i_maxS = 255;
+	//0-255
+	double i_minV = 55;
+	double i_maxV = 255;
+
+	while (1)
+	{
+		Mat frame;
+		Mat hsvMat;
+		Mat detectMat;
+
+		cap >> frame;
+		Size ResImgSize = Size(frame.cols*scale, frame.rows*scale);
+		Mat rFrame = Mat(ResImgSize, frame.type());
+		resize(frame, rFrame, ResImgSize, INTER_LINEAR);
+
+		cvtColor(rFrame, hsvMat, COLOR_BGR2HSV);
+
+		rFrame.copyTo(detectMat);
+
+		inRange(hsvMat, Scalar(i_minH, i_minS, i_minV), Scalar(i_maxH, i_maxS, i_maxV), detectMat);
+
+		imshow("while :in the range", detectMat);
+		imshow("frame", rFrame);
+
+		waitKey(30);
+	}
+
+	return 0;
+
+}
+
+
